30bf9658cce4cd4f263ddfbceb2663621c3f2cf1
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-08-13 17:59:59 +0100

    initial commit and project structure setup

---
+++ pages/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Portfolio from '../components/Portfolio';
+import Testimonials from '../components/Testimonials';
+import ContactUs from '../components/ContactUs';
+import Footer from '../components/Footer';
+import WhatWeDo from '../components/WhatWeDo';
+
+export { Navbar, Hero, Portfolio, Testimonials, ContactUs, Footer, WhatWeDo };
