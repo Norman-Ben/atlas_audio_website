7e8e031f14ee3b4ed171458ae6dcea017fd2d726
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-08-26 19:21:26 +0100

    add the WhatWeDo component with styling

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -5,10 +5,7 @@ import Image from 'next/future/image';
 
 export default function Hero() {
   return (
-    <section
-      id="home"
-      className={`flex lg:flex-row flex-col-reverse ${styles.paddingY}`}
-    >
+    <section id="home" className={`flex lg:flex-row flex-col-reverse py-3`}>
       {/* Hero Picture Container */}
       <div className={`${styles.flexCenter} flex-wrap lg:my-0 my-10`}>
         <Image
@@ -16,14 +13,14 @@ export default function Hero() {
           width={1024}
           height={683}
           alt="Sound engineer working on a mixing desk"
-          className="xl:w-[70%] lg:w-[55%] w-[80%] z-[5] top-6 rounded-[30px] mb-2"
+          className="xl:w-[70%] lg:w-[55%] w-[80%] z-[5] top-6 rounded-[15px] mb-2"
         />
         <Image
           src="/assets/Hero-Picture2.jpeg"
           width={1024}
           height={683}
           alt="Test"
-          className="xl:w-[70%] lg:w-[55%] w-[80%] z-[5] rounded-[30px] mt-2"
+          className="xl:w-[70%] lg:w-[55%] w-[80%] z-[5] rounded-[15px] mt-2"
         />
         {/* Gradients */}
 
@@ -38,7 +35,7 @@ export default function Hero() {
         <div
           className={`flex flex-row justify-between items-center w-full z-[5]`}
         >
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[42px] text-white ss:leading-[85px] leading-[60px]">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[42px] text-white ss:leading-[80px] leading-[60px]">
             Bespoke <span className="text-gradient">compositions</span> for
             media <br className="sm:block hidden" /> and professional{' '}
             <span className="text-gradient">audio services</span>.
--- pages/components/WhatWeDo.js
+++ pages/components/WhatWeDo.js
@@ -1,5 +1,81 @@
 import React from 'react';
+import styles, { layout } from '../style';
+import ContactUsButton from './ContactUsButton';
+import Image from 'next/future/image';
 
 export default function WhatWeDo() {
-  return <div>WhatWeDo</div>;
+  return (
+    <section>
+      <section className={layout.section}>
+        <div className={`${layout.sectionInfo} sm:pt-12`}>
+          <h2 className={`${styles.heading2} max-w-[600px]`}>
+            A range of musical services <br className="md:block hidden" />{' '}
+            tailored for your media.
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Looking for a snappy soundtrack for your podcast? Composition for
+            your RPG inspired game? Catchy background music for your
+            advertisement? We can help bring your media to life. Fully produced
+            within our professional studio, we’re able to provide exactly what
+            your media needs, regardless of genre or style.
+          </p>
+        </div>
+
+        <div className={`${layout.sectionImg}`}>
+          <Image
+            alt="Mixing Desk"
+            src="/assets/AtlasGuitarPic.jpg"
+            height={1080}
+            width={1080}
+            className="md:w-[70%] w-[65%] rounded-[15px]"
+          />
+        </div>
+      </section>
+      <section className={layout.sectionReverse}>
+        <div className={`${layout.sectionImgReverse}`}>
+          <Image
+            src="/assets/AtlasKeysPic.jpg"
+            alt="Guitar"
+            height={640}
+            width={640}
+            className="md:w-[70%] w-[65%] rounded-[15px] relative z-[5]"
+          />
+
+          {/* gradients */}
+          <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[70%] rounded-full white__gradient" />
+          <div className="absolute z-[0] w-[50%] h-[70%] -left-1/2 bottom-0 rounded-full purple__gradient" />
+        </div>
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>State of the art Studio</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Our studio, located in bath, is equipped with all the hardware
+            required to make your project execptional. From recording and
+            editing to mixing and mastering.
+          </p>
+        </div>
+      </section>
+      <section className={layout.section}>
+        <div className={`${layout.sectionInfo} sm:pt-12`}>
+          <h2 className={`${styles.heading2} max-w-[600px]`}>Pricing</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            We guarantee a personable and professional service, containing open
+            communication as well as a variety of pricing structures to suit
+            your needs and budget. Please use the slider below for a quote based
+            on total length of composition(s) required.
+          </p>
+          <ContactUsButton styles="mt-10" />
+        </div>
+
+        <div className={`${layout.sectionImg}`}>
+          <Image
+            alt="Mixing Desk"
+            src="/assets/AtlasMixingDeskPic.jpg"
+            height={1080}
+            width={1080}
+            className="md:w-[70%] w-[65%] rounded-[15px]"
+          />
+        </div>
+      </section>
+    </section>
+  );
 }
