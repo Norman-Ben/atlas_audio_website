49ec5df12f48580d58ecbf393f0e9693a3d497ff
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2022-10-28 11:59:16 +0100

    add utopian dream video to the portfolio page

--- pages/components/PortfolioContent.js
+++ pages/components/PortfolioContent.js
@@ -9,6 +9,16 @@ export default function PortfolioContent() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-7 h-full w-full">
           <div className="relative overflow-hidden pt-[56.25%]">
             <iframe
+              src="https://www.youtube-nocookie.com/embed/itGV60ndqMI"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className="absolute top-0 left-0 w-full h-full"
+            ></iframe>
+          </div>
+          <div className="relative overflow-hidden pt-[56.25%]">
+            <iframe
               src="https://www.youtube-nocookie.com/embed/YFHXiyJfAdc"
               title="YouTube video player"
               frameBorder="0"
