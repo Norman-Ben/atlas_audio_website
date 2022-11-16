d37d2ae2ff596d2db419a3db9f38d57600fc5d44
Norman-Ben <90580315+Norman-Ben@users.noreply.github.com>
2022-11-16 11:41:18 +0000

    Revert "git merge changes"
    This reverts commit 34ecd276a13ad8452a8cdca5ecad25f93523b448.

--- pages/components/BarnStudioContent.js
+++ pages/components/BarnStudioContent.js
@@ -72,16 +72,15 @@ export default function BarnStudioContent() {
             </div>
           </div>
         </div>
+
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>The Barn Studio</h2>
           <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Packed to the brim with new and old keyboards, guitars, basses,
+            amps, speakers, drums, mics & more. The Barn Studio is our
+            soundscape sandbox, and it can be yours too. Whether you’re looking
+            for a soundtrack for your game, or a place to come and record your
+            latest album, we do it all.
           </p>
         </div>
       </section>
